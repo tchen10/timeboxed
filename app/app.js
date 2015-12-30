robertvu483/My_CV

chore(protractor): update to use protractor v2.1.0

app/app.js
@@ -1,0 +1,12 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('myApp', [
+  'ngRoute',
+  'myApp.view1',
+  'myApp.view2',
+  'myApp.version'
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.otherwise({redirectTo: '/view1'});
+}]);

app/components/version/interpolate-filter.js
@@ -1,0 +1,9 @@
+'use strict';
+
+angular.module('myApp.version.interpolate-filter', [])
+
+.filter('interpolate', ['version', function(version) {
+  return function(text) {
+    return String(text).replace(/\%VERSION\%/mg, version);
+  };
+}]);

app/components/version/version-directive.js
@@ -1,0 +1,9 @@
+'use strict';
+
+angular.module('myApp.version.version-directive', [])
+
+.directive('appVersion', ['version', function(version) {
+  return function(scope, elm, attrs) {
+    elm.text(version);
+  };
+}]);

app/components/version/version.js
@@ -1,0 +1,8 @@
+'use strict';
+
+angular.module('myApp.version', [
+  'myApp.version.interpolate-filter',
+  'myApp.version.version-directive'
+])
+
+.value('version', '0.1');

tchen10/timeboxed

app/app.js
@@ -1,0 +1,12 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('timeboxed', [
+  'ngRoute',
+  'timeboxed.view1',
+  'timeboxed.view2',
+  'timeboxed.version'
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.otherwise({redirectTo: '/view1'});
+}]);

app/components/version/interpolate-filter.js
@@ -1,0 +1,9 @@
+'use strict';
+
+angular.module('timeboxed.version.interpolate-filter', [])
+
+.filter('interpolate', ['version', function(version) {
+  return function(text) {
+    return String(text).replace(/\%VERSION\%/mg, version);
+  };
+}]);

app/components/version/version-directive.js
@@ -1,0 +1,9 @@
+'use strict';
+
+angular.module('timeboxed.version.version-directive', [])
+
+.directive('appVersion', ['version', function(version) {
+  return function(scope, elm, attrs) {
+    elm.text(version);
+  };
+}]);

app/components/version/version.js
@@ -1,0 +1,8 @@
+'use strict';
+
+angular.module('timeboxed.version', [
+  'timeboxed.version.interpolate-filter',
+  'timeboxed.version.version-directive'
+])
+
+.value('version', '0.1');